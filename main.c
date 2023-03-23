c90b4b4a387425df8ced55533c55ed12155726e1
PotatoOverlord22 <raul.tiron@stud.ubbcluj.ro>
2023-03-23 14:19:21 +0200

    ADDED: service tests

--- main.c
+++ main.c
@@ -6,15 +6,17 @@
 
 
 int main() {
-//    country_tests();
-//    vector_tests();
-//    repository_tests();
-    Repository* repository = repository_create();
-    Service* service = service_create(repository);
-    service_initialize_hard_coded_countries(service);
-    UI* ui = ui_create(service);
-    start_menu(ui);
+    country_tests();
+    vector_tests();
+    repository_tests();
+    service_tests();
 
-    ui_destroy(ui);
+//    Repository* repository = repository_create();
+//    Service* service = service_create(repository);
+//    service_initialize_hard_coded_countries(service);
+//    UI* ui = ui_create(service);
+//    start_menu(ui);
+//
+//    ui_destroy(ui);
     return 0;
 }
--- tests/service_tests.c
+++ tests/service_tests.c
@@ -1,3 +1,16 @@
-//
-// Created by raul on 22.03.2023.
-//
+#include "../service/service.h"
+#include <assert.h>
+
+void service_tests(){
+    Repository* repository1 = repository_create();
+    Service* service1 = service_create(repository1);
+    service_add_country(service1, "test-name1", "test-continent1", 10.01);
+    service_add_country(service1, "test-name2", "test-continent2", 20.02);
+    assert(service_get_repository_size(service1) == 2);
+    service_remove_country_by_name(service1, "test-name1");
+    assert(!service_remove_country_by_name(service1, "not in repo"));
+    assert(service_get_repository_size(service1) == 1);
+    service_update_country_name(service1, "test-name2", "new-test-name2");
+
+    service_destroy(service1);
+}
